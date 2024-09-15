a46c0d6792338a629a5810b2e77eef9075484454
dogruvolkan <volkandogru11@gmail.com>
2024-09-16 00:05:16 +0300

    deploy fixed

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { Habit } from "../types";
 import HabitFormModal from "./HabitFormModal";
 import "../styles/HabitList.css";
